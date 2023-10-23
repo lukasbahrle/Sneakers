729ed20092ac82cfa363149b3ba222ec36f272c8
Lukas Bahrle Santana <lukas.bahrle@bestsecret.com>
2023-10-23 21:39:08 +0200

    Initial Commit

---
+++ Sneakers/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Sneakers
+//
+//  Created by Bahrle, Lukas on 23/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Sneakers/SneakersApp.swift
@@ -0,0 +1,17 @@
+//
+//  SneakersApp.swift
+//  Sneakers
+//
+//  Created by Bahrle, Lukas on 23/10/23.
+//
+
+import SwiftUI
+
+@main
+struct SneakersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
